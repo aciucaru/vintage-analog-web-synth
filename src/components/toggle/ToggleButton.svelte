8d447abc19411a1febbf8da771df7b446c748a60
aciucaru <aciukru@gmail.com>
2024-06-23 15:55:42 +0300

    Switched to inline CSS for width and height of ToggleButton.

--- src/components/toggle/ToggleButton.svelte
+++ src/components/toggle/ToggleButton.svelte
@@ -9,15 +9,15 @@
     // prop that tells if the toggle button starts in On (true) or Off (false) mode
     export let isToggled: boolean = false;
 
+    // props for width and heigth of button
+    export let buttonWidth = 45;
+    export let buttonHeight = 30;
+
     // the classes for the background and foreground of the button
     let backgroundClass = "button-image background-off-default-image";
     let foregroundClass = "button-image foreground-off-default-image foreground-off-filter";
 
-    let buttonWidth = 0;
-    let buttonHeight = 0;
-
-
-    // the event handler for the 'click' event, this methods gets called when the button is clicked
+    // the event handler for the 'click' event, this methods gets called when the toggle button is clicked
     function handleToggleClick(): void
     {
         // switch toggled state
@@ -31,44 +31,28 @@
     $: foregroundClass = isToggled ? "button-image foreground-on-default-image foreground-on-filter" : "button-image foreground-off-default-image foreground-off-filter";
 </script>
 
-<!-- <div style={`--button-width: ${buttonWidth}px; button-height: ${buttonHeight}px;`} class="main-container"> -->
-<div class="main-container">
-    <!-- the toggle button -->
-    <!-- <div class="button-container">
-        <div class="button-main">
-            <slot name="bgOnImage">
-                <div class={backgroundClass}></div>
-            </slot>
-        </div>
-    
-        <div class="button-main" on:click={handleToggleClick}>
-            <slot name="fgOnImage">
-                <div class={foregroundClass}></div>
-            </slot>
-        </div>
-    </div> -->
-
-    <div class="button-container">
-        <div class="button-main">
+<div class="main-container" style={`width: ${buttonWidth}px; height: ${buttonHeight}px;`}>
+    <div class="button-container" style={`width: ${buttonWidth}px; height: ${buttonHeight}px;`}>
+        <div class="button-main" style={`width: ${buttonWidth}px; height: ${buttonHeight}px;`}>
             {#if isToggled}
                 <slot name="bgOnImage">
-                    <div class="button-image background-on-default-image"></div>
+                    <div class="button-image background-on-default-image" style={`width: ${buttonWidth}px; height: ${buttonHeight}px;`}></div>
                 </slot>
             {:else}
                 <slot name="bgOffImage">
-                    <div class="button-image background-off-default-image"></div>
+                    <div class="button-image background-off-default-image" style={`width: ${buttonWidth}px; height: ${buttonHeight}px;`}></div>
                 </slot>
             {/if}
         </div>
     
-        <div class="button-main" on:click={handleToggleClick}>
+        <div class="button-main" style={`width: ${buttonWidth}px; height: ${buttonHeight}px;`} on:click={handleToggleClick}>
             {#if isToggled}
                 <slot name="fgOnImage">
-                    <div class="button-image foreground-on-default-image foreground-on-filter"></div>
+                    <div class="button-image foreground-on-default-image foreground-on-filter" style={`width: ${buttonWidth}px; height: ${buttonHeight}px;`}></div>
                 </slot>
             {:else}
                 <slot name="fgOffImage">
-                    <div class="button-image foreground-off-default-image foreground-off-filter"></div>
+                    <div class="button-image foreground-off-default-image foreground-off-filter" style={`width: ${buttonWidth}px; height: ${buttonHeight}px;`}></div>
                 </slot>
             {/if}
         </div>
@@ -85,8 +69,6 @@
 <style>
     .main-container
     {
-        --button-width: 45px;
-        --button-height: 30px;
         --text-height: 12px;
 
         box-sizing: border-box;
@@ -106,10 +88,7 @@
 
     .button-container
     {
-        /* width and height are necessary in order to display the background image */
-        width: var(--button-width);
-        height: var(--button-height);
-
+        /* width and height are necessary in order to display the background image, but here they come from inline styles */
         width: auto;
         height: auto;
 
@@ -128,9 +107,7 @@
 
     .button-main
     {
-        /* width and height are necessary in order to display the background image */
-        width: var(--button-width);
-        height: var(--button-height);
+        /* width and height are necessary in order to display the background image, but here they come from inline styles */
 
         grid-column: 1 / 2;
         grid-row: 1 / 2;
@@ -143,9 +120,7 @@
 
     .button-image
     {
-        /* width and height are necessary in order to display the background image */
-        width: var(--button-width);
-        height: var(--button-height);
+        /* width and height are necessary in order to display the background image, but here they come from inline styles */
 
         padding: 0px;
         margin: 0px;
@@ -163,7 +138,6 @@
     /* classes for default button images */
     .background-on-default-image
     {
-        /* background-image: url("$lib/rocker-bg-on-opt.svg"); */
         background-image: url("../../assets/toggle-button/rocker-bg-on-opt.svg");
     }
 
@@ -223,23 +197,6 @@
         overflow: hidden;
         white-space: nowrap;
         text-overflow: clip;
-
-        margin: 0px;
-        margin-left: 10px;
-    }
-
-    .icon
-    {
-        /* width and height are necessary in order to display the background image */
-        width: 24px;
-        height: 12px;
-
-        background-size: 100% auto;
-        background-size: contain;
-        background-attachment: scroll;
-        background-repeat: no-repeat;
-        background-position: top left;
-        filter: brightness(1.5);
 
         margin: 0px;
         margin-left: 10px;
