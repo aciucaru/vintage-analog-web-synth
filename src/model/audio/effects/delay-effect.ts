fbfcb85f6019dd80ebbf82518ecfccea59289f78
aciucaru <aciukru@gmail.com>
2024-07-14 17:14:56 +0300

    Created base class for all effects.

--- src/model/audio/effects/delay-effect.ts
+++ src/model/audio/effects/delay-effect.ts
@@ -1,31 +1,15 @@
 import { Settings } from "../../../constants/settings";
-import { SingleInputBaseAudioNode } from "../single-input-base-audio-node";
+import { BaseEffect } from "./base-effect";
 
 import { Logger } from "tslog";
 import type { ILogObj } from "tslog";
 
 
-export class DelayEffect extends SingleInputBaseAudioNode
+export class DelayEffect extends BaseEffect
 {
-    // the input node of this effect
-    private inputNode: AudioNode | null = null;
-
-    private isEffectEnabled = false;
-
-    // atenuators for input and delay, these help obtain the on/off effect (they help turn the effect on/off)
-    private inputOnOffGainNode: GainNode;
-    private effectOnOffGainNode: GainNode;
-
-    // atenuators for input and delay, these help obtain the wet/dry effect
-    private inputWetDryGainNode: GainNode;
-    private effectWetDryGainNode: GainNode;
-
     // the delay node itself and a feedback node
     private delayNode: DelayNode;
     private delayFeedabackNode: GainNode;
-
-    // the final output ot this effect
-    private outputGainNode: GainNode;
 
     private static readonly logger: Logger<ILogObj> = new Logger({name: "DelayEffect", minLevel: Settings.minLogLevel });
 
@@ -33,16 +17,6 @@ export class DelayEffect extends SingleInputBaseAudioNode
     {
         super(audioContext);
 
-        this.inputOnOffGainNode = this.audioContext.createGain();
-        this.inputOnOffGainNode.gain.setValueAtTime(Settings.maxEffectOnOffGain, this.audioContext.currentTime);
-        this.effectOnOffGainNode = this.audioContext.createGain();
-        this.effectOnOffGainNode.gain.setValueAtTime(Settings.minEffectWetDryGain, this.audioContext.currentTime);
-
-        this.inputWetDryGainNode = this.audioContext.createGain();
-        this.inputWetDryGainNode.gain.setValueAtTime(Settings.defaultEffectWetDryGain, this.audioContext.currentTime);
-        this.effectWetDryGainNode = this.audioContext.createGain();
-        this.effectWetDryGainNode.gain.setValueAtTime(Settings.defaultEffectWetDryGain, this.audioContext.currentTime);
-        
         this.delayNode = this.audioContext.createDelay();
         this.delayNode.delayTime.setValueAtTime(Settings.minDelayTime, this.audioContext.currentTime);
 
@@ -65,46 +39,6 @@ export class DelayEffect extends SingleInputBaseAudioNode
         // connect atenuators to final output gain node
         this.inputWetDryGainNode.connect(this.outputGainNode);
         this.effectWetDryGainNode.connect(this.outputGainNode);
-    }
-
-    public connectInput(inputNode: AudioNode): void
-    {
-        this.inputNode = inputNode;
-
-        // connect the input node to the delay and also to the main output node
-        this.inputNode.connect(this.effectOnOffGainNode);
-        this.inputNode.connect(this.inputOnOffGainNode);
-    }
-
-    public outputNode(): AudioNode { return this.outputGainNode; }
-
-    // this method toggles the effect on/off (it enables or disables the effect)
-    public toggleEffect(): void
-    {
-        this.isEffectEnabled = !this.isEffectEnabled;
-
-        const currentTime = this.audioContext.currentTime;
-
-        if (this.isEffectEnabled)
-        {
-            DelayEffect.logger.debug(`toggleEffect(): on`);
-
-            // set the input route (dry signal route) gain to min
-            this.inputOnOffGainNode.gain.linearRampToValueAtTime(Settings.minEffectOnOffGain, currentTime);
-
-            // set the effect route (wet signal route) gain to max
-            this.effectOnOffGainNode.gain.linearRampToValueAtTime(Settings.maxEffectOnOffGain, currentTime);
-        }
-        else
-        {
-            DelayEffect.logger.debug(`toggleEffect(): off`);
-
-            // set the input route (dry signal route) gain to max
-            this.inputOnOffGainNode.gain.linearRampToValueAtTime(Settings.maxEffectOnOffGain, currentTime);
-
-            // set the effect route (wet signal route) gain to min
-            this.effectOnOffGainNode.gain.linearRampToValueAtTime(Settings.minEffectOnOffGain, currentTime);
-        }
     }
 
     public setDelayTime(delayTime: number): boolean
@@ -139,38 +73,6 @@ export class DelayEffect extends SingleInputBaseAudioNode
         {
             DelayEffect.logger.warn(`setFeedbackLevel(${feedback}): parameter is outside bounds`);
             return false; // change was succesfull
-        }
-    }
-
-    /* This method sets the wet/dry level of the delay effect. It basically sets how much of the signal
-    ** coming from the delay effect is supposed to be heard along the original signal.
-    ** The original signal is called 'dry' (no effects) and the signal comming out of the delay effect
-    ** is called 'wet' (because it has effects).
-    **
-    ** In order to obtain the wet/dry capability, the dry audio input is passed through a gain node and the
-    ** wet signal comming out of the delay effect is also passed through a gain node and these two gain nodes
-    ** are then combined. Each gain node has it's own gain value, so it has it's own weight, which gives the illusion
-    ** that we can teak the amount of effect (the wet/dry level).
-    **
-    ** The 'effectAmount' argument represents the weight of the signal comming out of the delay effect, while
-    ** the weight of the original ('dry') signal is 100% minus the weight of the 'wet' signal. */
-    public setEffectAmount(effectAmount: number): boolean
-    {
-        if (Settings.minEffectWetDryGain <= effectAmount && effectAmount <= Settings.maxEffectWetDryGain)
-        {
-            DelayEffect.logger.debug(`setEffectAmount(${effectAmount})`);
-
-            const currentTime = this.audioContext.currentTime;
-
-            this.inputWetDryGainNode.gain.linearRampToValueAtTime(Settings.maxEffectWetDryGain - effectAmount, currentTime);
-            this.effectWetDryGainNode.gain.linearRampToValueAtTime(effectAmount, currentTime);
-
-            return true; // change was succesfull
-        }
-        else
-        {
-            DelayEffect.logger.warn(`setEffectAmount(${effectAmount}): parameter is outside bounds`);
-            return false; // change was not succesfull
         }
     }
 }
--- src/model/audio/effects/distortion-effect.ts
+++ src/model/audio/effects/distortion-effect.ts
@@ -10,26 +10,27 @@
 
 
 import { Settings } from "../../../constants/settings";
-import { SingleInputBaseAudioNode } from "../single-input-base-audio-node";
+import { BaseEffect } from "./base-effect";
 
 import { Logger } from "tslog";
 import type { ILogObj } from "tslog";
 
 
-export class DistortionEffect extends SingleInputBaseAudioNode
+
+export class DistortionEffect extends BaseEffect
 {
     // the input node of this effect
-    private inputNode: AudioNode | null = null;
+    // private inputNode: AudioNode | null = null;
 
-    private isEffectEnabled = false;
+    // private isEffectEnabled = false;
 
     // atenuators for input and delay, these help obtain the on/off effect (they help turn the effect on/off)
-    private inputOnOffGainNode: GainNode;
-    private effectOnOffGainNode: GainNode;
+    // private inputOnOffGainNode: GainNode;
+    // private effectOnOffGainNode: GainNode;
 
     // atenuators for input and delay, these help obtain the wet/dry effect
-    private inputWetDryGainNode: GainNode;
-    private effectWetDryGainNode: GainNode;
+    // private inputWetDryGainNode: GainNode;
+    // private effectWetDryGainNode: GainNode;
 
     // the delay node itself and a feedback node
     private distortionNode: WaveShaperNode;
@@ -38,7 +39,7 @@ export class DistortionEffect extends SingleInputBaseAudioNode
     private distortionConstantValue: number = Settings.defaultDistortionCurveConstantValue;
 
     // the final output ot this effect
-    private outputGainNode: GainNode;
+    // private outputGainNode: GainNode;
 
     private static readonly CURVE_SAMPLES_COUNT = 200;
 
@@ -47,16 +48,6 @@ export class DistortionEffect extends SingleInputBaseAudioNode
     constructor(audioContext: AudioContext)
     {
         super(audioContext);
-
-        this.inputOnOffGainNode = this.audioContext.createGain();
-        this.inputOnOffGainNode.gain.setValueAtTime(Settings.maxEffectOnOffGain, this.audioContext.currentTime);
-        this.effectOnOffGainNode = this.audioContext.createGain();
-        this.effectOnOffGainNode.gain.setValueAtTime(Settings.minEffectWetDryGain, this.audioContext.currentTime);
-
-        this.inputWetDryGainNode = this.audioContext.createGain();
-        this.inputWetDryGainNode.gain.setValueAtTime(Settings.defaultEffectWetDryGain, this.audioContext.currentTime);
-        this.effectWetDryGainNode = this.audioContext.createGain();
-        this.effectWetDryGainNode.gain.setValueAtTime(Settings.defaultEffectWetDryGain, this.audioContext.currentTime);
         
         this.distortionNode = this.audioContext.createWaveShaper();
         this.distortionNode.curve = this.makeDistortionCurve(this.distortionAmount, this.distortionAngle, this.distortionConstantValue);
@@ -76,18 +67,6 @@ export class DistortionEffect extends SingleInputBaseAudioNode
         this.inputWetDryGainNode.connect(this.outputGainNode);
     }
 
-    public connectInput(inputNode: AudioNode): void
-    {
-        this.inputNode = inputNode;
-
-        // connect the input node to the delay and also to the main output node
-        this.inputNode.connect(this.effectOnOffGainNode);
-        this.inputNode.connect(this.inputOnOffGainNode);
-    }
-
-    public outputNode(): AudioNode { return this.outputGainNode; }
-
-    // best version
     private makeDistortionCurve(amount: number, angle: number, constantValue: number): Float32Array
     {
         const curveSamples = new Float32Array(DistortionEffect.CURVE_SAMPLES_COUNT);
@@ -106,35 +85,6 @@ export class DistortionEffect extends SingleInputBaseAudioNode
         }
 
         return curveSamples;
-    }
-
-    // this method toggles the effect on/off (it enables or disables the effect)
-    public toggleEffect(): void
-    {
-        this.isEffectEnabled = !this.isEffectEnabled;
-
-        const currentTime = this.audioContext.currentTime;
-
-        if (this.isEffectEnabled)
-        {
-            DistortionEffect.logger.debug(`toggleEffect(): on`);
-
-            // set the input route (dry signal route) gain to min
-            this.inputOnOffGainNode.gain.linearRampToValueAtTime(Settings.minEffectOnOffGain, currentTime + 0.05);
-
-            // set the effect route (wet signal route) gain to max
-            this.effectOnOffGainNode.gain.linearRampToValueAtTime(Settings.maxEffectOnOffGain, currentTime + 0.05);
-        }
-        else
-        {
-            DistortionEffect.logger.debug(`toggleEffect(): off`);
-
-            // set the input route (dry signal route) gain to max
-            this.inputOnOffGainNode.gain.linearRampToValueAtTime(Settings.maxEffectOnOffGain, currentTime + 0.05);
-
-            // set the effect route (wet signal route) gain to min
-            this.effectOnOffGainNode.gain.linearRampToValueAtTime(Settings.minEffectOnOffGain, currentTime + 0.05);
-        }
     }
 
     public setDistortionAmount(distortionAmount: number): boolean
@@ -190,38 +140,6 @@ export class DistortionEffect extends SingleInputBaseAudioNode
         else
         {
             DistortionEffect.logger.warn(`setDistortionCurveConstantValue(${distortionCurveConstantValue}): parameter is outside bounds`);
-            return false; // change was not succesfull
-        }
-    }
-
-    /* This method sets the wet/dry level of the distortion effect. It basically sets how much of the signal
-    ** coming from the effect is supposed to be heard along the original signal.
-    ** The original signal is called 'dry' (no effects) and the signal comming out of the effect
-    ** is called 'wet' (because it has effects).
-    **
-    ** In order to obtain the wet/dry capability, the dry audio input is passed through a gain node and the
-    ** wet signal comming out of the effect is also passed through a gain node and these two gain nodes
-    ** are then combined. Each gain node has it's own gain value, so it has it's own weight, which gives the illusion
-    ** that we can teak the amount of effect (the wet/dry level).
-    **
-    ** The 'effectAmount' argument represents the weight of the signal comming out of the delay effect, while
-    ** the weight of the original ('dry') signal is 100% minus the weight of the 'wet' signal. */
-    public setEffectAmount(effectAmount: number): boolean
-    {
-        if (Settings.minEffectWetDryGain <= effectAmount && effectAmount <= Settings.maxEffectWetDryGain)
-        {
-            DistortionEffect.logger.debug(`setEffectAmount(${effectAmount})`);
-
-            const currentTime = this.audioContext.currentTime;
-
-            this.inputWetDryGainNode.gain.linearRampToValueAtTime(Settings.maxEffectWetDryGain - effectAmount, currentTime);
-            this.effectWetDryGainNode.gain.linearRampToValueAtTime(effectAmount, currentTime);
-
-            return true; // change was succesfull
-        }
-        else
-        {
-            DistortionEffect.logger.warn(`setEffectAmount(${effectAmount}): parameter is outside bounds`);
             return false; // change was not succesfull
         }
     }
