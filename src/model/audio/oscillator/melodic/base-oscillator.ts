9399928da321e122e86a2617af99a8704c226b6a
aciucaru <aciukru@gmail.com>
2024-07-28 12:40:18 +0300

    Cleaned up code.

--- src/model/audio/oscillator/melodic/base-oscillator.ts
+++ src/model/audio/oscillator/melodic/base-oscillator.ts
@@ -14,7 +14,7 @@ export abstract class BaseOscillator
     ** this gain is always at max. level, so the drawing is full size */
     protected analyserGainNode: GainNode;
 
-    // the final output of the oscillator; this is used to connect he oscillator to other nodes
+    // the final output of the oscillator; this is used to connect the oscillator to other nodes
     protected outputGainNode: GainNode;
 
     private static readonly baseOscLogger: Logger<ILogObj> = new Logger({name: "BaseOscillator", minLevel: Settings.minLogLevel });
@@ -38,26 +38,6 @@ export abstract class BaseOscillator
         this.outputGainNode = this.audioContext.createGain();
         this.outputGainNode.gain.setValueAtTime(Settings.defaultOscGain, this.audioContext.currentTime);
     }
-
-    // sets the value of the main gain node
-    // public setOutputGain(gain: number): boolean
-    // {
-    //     if (Settings.minOscGain <= gain && gain <= Settings.maxOscGain)
-    //     {
-    //         BaseOscillator.baseOscLogger.debug(`setOutputGain(${gain})`);
-
-    //         // set the new value
-    //         this.outputGainNode.gain.linearRampToValueAtTime(gain, this.audioContext.currentTime);
-
-    //         return true; // change was successfull
-    //     }
-    //     else
-    //     {
-    //         BaseOscillator.baseOscLogger.warn(`setOutputGain(${gain}): value outside bounds`);
-
-    //         return false; // change was not successfull
-    //     }
-    // }
 
     // returns the main gain node
     public outputNode(): GainNode { return this.outputGainNode; }
