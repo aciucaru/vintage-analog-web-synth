ef26ef586d7965147b363dff5ee6121be8e801d4
aciucaru <aciukru@gmail.com>
2024-07-28 12:11:13 +0300

    Cleaned up ADSR envelope code.

--- src/model/audio/modulation/adsr-envelope.ts
+++ src/model/audio/modulation/adsr-envelope.ts
@@ -1,6 +1,5 @@
 import { Settings } from "../../../constants/settings";
 import { InputOutputBaseAudioNode } from "../base/input-output-base-audio-node";
-import { NoInputBaseAudioNode } from "../base/no-input-base-audio-node";
 
 import { Logger } from "tslog";
 import type { ILogObj } from "tslog";
@@ -54,26 +53,6 @@ export class AdsrEnvelope extends InputOutputBaseAudioNode
         // connect inherited input and output to the low-pass filter node
         this.inputGainNode.connect(this.adsrGainNode);
         this.adsrGainNode.connect(this.outputGainNode);
-    }
-
-    /* implementation of 'mainNode()', the only method of the BaseAudioNode abstract class
-    ** this method is supposed to return the main node of the class */
-    // public mainNode(): AudioNode { return this.adsrGainNode; }
-
-    // this method is for sequencer beats (steps)
-    public startBeat(duration: number): void
-    {
-        let timeDuration = 0;
-
-        if (duration > 0)
-            timeDuration = duration;
-        else
-            timeDuration = (60.0 / 120.0) / 4.0; // clip to 120 BPM 4/4
-
-        const currentTime = this.audioContext.currentTime; // the time when note was triggered
-
-        this.startAtTime(currentTime);
-        this.stopAtTime(currentTime + timeDuration);
     }
 
     /* This method represents the ADS portion of the envelope, it basically coressponds to the 'noteOn' event */
@@ -131,11 +110,18 @@ export class AdsrEnvelope extends InputOutputBaseAudioNode
         this.adsrGainNode.gain.linearRampToValueAtTime(Settings.minAdsrSustainLevel, this.releaseEndTime);
     }
 
-    /* This method represents the ADS portion of the ADSR envelope, but, unlike the previous 'start()'
-    ** method, this method starts the ADS event at a specific time */
-    public startAtTime(startTime: number): void
+    // this method is for sequencer beats (steps)
+    public startBeat(duration: number): void
     {
-        AdsrEnvelope.logger.debug(`startAtTime(): ADSR triggered`);
+        let timeDuration = 0;
+
+        if (duration > 0)
+            timeDuration = duration;
+        else
+            timeDuration = (60.0 / 120.0) / 4.0; // default to 120 BPM 4/4
+
+        const startTime = this.audioContext.currentTime; // the time when note was triggered
+        const stopTime = startTime + timeDuration;
 
         /* It might be possible that the attack-decay-sustain phase has started before the previous ADSR event has finished.
         ** If we just add an 'attack-decay-sustain' event and not cancel all remaining events, the remaining events will trigger
@@ -149,40 +135,17 @@ export class AdsrEnvelope extends InputOutputBaseAudioNode
         ** that the parameter had when the cancelation started */ 
         this.adsrGainNode.gain.cancelAndHoldAtTime(cancelationStartTime);
 
-        /* After we checked the current ADSR phase based on ADSR times, we can now compute and overwrite ADSR times
-        ** with new values.
-        ** Overwriting the ADSR times should only be done after we used them to check what phase we are in!
-        ** These new ADSR times will be used in the stop() method as well. */
+        /* Attack-decay-sustain phase */
         this.attackStartTime = cancelationStartTime; // save the attack start time
         this.attackEndTime = this.attackStartTime + this.attackDuration; // the time the attack phase should end
         this.decayEndTime = this.attackEndTime + this.decayDuration; // the time the decay phase should end
-
-        /* Attack and decay phases */
         this.adsrGainNode.gain.linearRampToValueAtTime(Settings.maxAdsrSustainLevel, this.attackEndTime); // attack
         this.adsrGainNode.gain.linearRampToValueAtTime(this.sustainLevel, this.decayEndTime); // decay
-    }
 
 
-    /* This method represents the R (release) portion of the ADSR envelope, but, unlike the previous 'stop()'
-    ** method, this method starts the R event at a specific time */
-    public stopAtTime(stopTime: number): void
-    {
-        /* It might be possible that the release phase has started before the previous ADSR event has finished.
-        ** If we just add a 'release' event and not cancel all remaining events, the remaining events will trigger
-        ** as well, because, chronologically, they are scheduled AFTER this 'release' event.
-        ** In order to prevent this bug, we use 'cancelAndHoldAtTime()'. */
-
-        // now we compute the time AFTER all scheduled events should be canceled
-        const cancelationStartTime = stopTime;
-
-        // cancel all remaining events
-        this.adsrGainNode.gain.cancelAndHoldAtTime(cancelationStartTime);
-
-        // compute the start and end of the 'release' phase
-        this.releaseStartTime = cancelationStartTime;
+        /* Release phase */
+        this.releaseStartTime = stopTime;
         this.releaseEndTime = this.releaseStartTime + this.releaseDuration;
-
-        // then start the actual 'release' phase by ramping down to the minimum possible
         // for 'release' phase we use linear ramp, not exponential, because exponential goes down to quick
         this.adsrGainNode.gain.linearRampToValueAtTime(Settings.minAdsrSustainLevel, this.releaseEndTime);
     }
