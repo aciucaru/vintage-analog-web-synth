4e65b9bd5ec3fb8174c89a6e049fb53a5b03c3e8
aciucaru <aciukru@gmail.com>
2024-08-04 21:18:12 +0300

    Added interface for analizable nodes (for use with AnalyserNode).

---
+++ src/model/audio/base/base-analizable-node.ts
@@ -0,0 +1,8 @@
+export interface BaseOscillator 
+{
+    /* the gain node that should be used for drawing the sound (for AnalyserNode)
+    ** this gain should be returned by the 'getAnalyserGainNode()' method */
+    analyserGainNode: GainNode;
+
+    getAnalyserGainNode(): GainNode;
+}
