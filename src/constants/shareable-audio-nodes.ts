fbc12c9e529b17c10a90d3b99fa26dc9f6598461
aciucaru <aciukru@gmail.com>
2024-08-08 20:37:40 +0300

    Cleaned up code.

--- src/constants/shareable-audio-nodes.ts
+++ src/constants/shareable-audio-nodes.ts
@@ -1,12 +1,1 @@
-import { Settings } from "./settings";
-import { UnipolarLfo } from "../model/audio/emitter-node/modulators/unipolar-lfo";
-
 export const audioContext = new AudioContext();
-
-// const lfoArray: Array<UnipolarLfo> = new Array<UnipolarLfo>(Settings.lfoCount);
-
-// for (let i = 0; i < lfoArray.length; i++)
-// {
-//     lfoArray[i] = new UnipolarLfo(audioContext);
-//     lfoArray[i].setFrequency(Settings.defaultLfoLowAbsoluteFrequency);
-// }
